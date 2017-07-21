df29e24b2ce3acfeb95705dfa32a68b80169b161
LightsOnHudson <bshaver@OfficeMini.home.lan>
2017-07-21 15:06:54 -0600

    Config help

---
+++ help/plugin_setup.php
@@ -0,0 +1,14 @@
+<b>Dynamic DNS Help</b>
+
+<p>
+Welcome to the Dynamic DNS Plugin. <br/>
+Navigate to this page: http://duckdns.org for setting up an account with DuckDNS.</a><br/>
+<br/>
+Once the account is created, you will need to get enter in the domain name (without the .duckdns.org) suffix into the plugin<br/>
+<br/>
+You will also need to add the token into the API Token field that is presented to you upon account creation.<br/>
+<br/>
+
+<img src="duckdnsConfig.png">
+
+
